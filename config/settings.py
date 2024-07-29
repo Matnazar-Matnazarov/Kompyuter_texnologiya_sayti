35b38ffa0576cc1cc58d29cb60d8887bb321b489
Dasturchi04 <matnazarmatnazarov3gmail.com>
2024-07-29 13:49:42 +0500

    upted collectstatic

--- config/settings.py
+++ config/settings.py
@@ -120,13 +120,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+import os
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[BASE_DIR / 'static/']
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 MEDIA_URL='/media/'
-MEDIA_ROOT= BASE_DIR/'media/' #'media_files'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 # Default primary key field type
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
